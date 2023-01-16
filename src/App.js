01ed30574f895af02747fc7f45664a85914ddd11
keynene <nonung2@naver.com>
2023-01-16 16:43:27 +0900

    [Project] Nav바 브랜드 경로수정

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
       {/* Navbar */}
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="https://keynene.github.io/Noinara/" >NOI ·_· NARA</Navbar.Brand>
+          <Navbar.Brand onClick={()=>{ navigate('https://keynene.github.io/Noinara/') }} >NOI ·_· NARA</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={()=>{ navigate('https://keynene.github.io/Noinara/') }} >Home</Nav.Link>
             <Nav.Link onClick={()=>{ navigate('/detail/0') }} >Detail</Nav.Link>
